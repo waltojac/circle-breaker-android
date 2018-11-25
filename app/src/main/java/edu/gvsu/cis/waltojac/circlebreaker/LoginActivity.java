cf23739eafc3d037b610603f534bc49398d7a33f
Jacob Walton <waltojac@mail.gvsu.edu>
2018-11-25 14:52:41 -0500

    Add logout button.

---
+++ app/src/main/java/edu/gvsu/cis/waltojac/circlebreaker/LoginActivity.java
@@ -0,0 +1,75 @@
+package edu.gvsu.cis.waltojac.circlebreaker;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class LoginActivity extends AppCompatActivity {
+
+    // Choose authentication providers
+    List<AuthUI.IdpConfig> providers = Arrays.asList(
+            new AuthUI.IdpConfig.EmailBuilder().build(),
+            new AuthUI.IdpConfig.GoogleBuilder().build());
+    FirebaseUser user;
+    public static final int RC_SIGN_IN = 1;
+    private boolean signedIn = false;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // User is signed out
+        Log.d("Auth:", "onAuthStateChanged:signed_out");
+        // Create and launch sign-in intent
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .build(),
+                RC_SIGN_IN);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+
+            if (resultCode == RESULT_OK) {
+                // Successfully signed in
+                user = FirebaseAuth.getInstance().getCurrentUser();
+                Toast.makeText(this, "Sign-in worked",
+                        Toast.LENGTH_LONG).show();
+                Intent intent = new Intent();
+                setResult(MainActivity.RC_SIGN_IN ,intent);
+                finish();
+                // ...
+            } else {
+                // Sign in failed. If response is null the user canceled the
+                // sign-in flow using the back button. Otherwise check
+                // response.getError().getErrorCode() and handle the error.
+                // ...
+                Toast.makeText(this, "Sign-in failed",
+                        Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+}
--- app/src/main/java/edu/gvsu/cis/waltojac/circlebreaker/MainActivity.java
+++ app/src/main/java/edu/gvsu/cis/waltojac/circlebreaker/MainActivity.java
@@ -1,12 +1,18 @@
 package edu.gvsu.cis.waltojac.circlebreaker;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.IdpResponse;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -15,50 +21,47 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    // Choose authentication providers
-    List<AuthUI.IdpConfig> providers = Arrays.asList(
-            new AuthUI.IdpConfig.EmailBuilder().build(),
-            new AuthUI.IdpConfig.GoogleBuilder().build());
     public static final int RC_SIGN_IN = 1;
 
+    FirebaseUser user;
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null) {
+            Toast.makeText(this, "On Main",
+                    Toast.LENGTH_LONG).show();
+        } else {
+            Intent i = new Intent(MainActivity.this, LoginActivity.class);
+            startActivityForResult(i, RC_SIGN_IN);
+        }
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Create and launch sign-in intent
-        startActivityForResult(
-                AuthUI.getInstance()
-                        .createSignInIntentBuilder()
-                        .setAvailableProviders(providers)
-                        .build(),
-                RC_SIGN_IN);
-    }
-
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if (requestCode == RC_SIGN_IN) {
-            IdpResponse response = IdpResponse.fromResultIntent(data);
-
-            if (resultCode == RESULT_OK) {
-                // Successfully signed in
-                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-                Toast.makeText(this, "Sign-in worked",
-                        Toast.LENGTH_LONG).show();
-                // ...
-            } else {
-                // Sign in failed. If response is null the user canceled the
-                // sign-in flow using the back button. Otherwise check
-                // response.getError().getErrorCode() and handle the error.
-                // ...
-                Toast.makeText(this, "Sign-in failed",
-                        Toast.LENGTH_LONG).show();
+        Button logOutButton = (Button) findViewById(R.id.logOutButton);
+        logOutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+                Intent i = new Intent(MainActivity.this, LoginActivity.class);
+                startActivityForResult(i, RC_SIGN_IN);
             }
-        }
+        });
+
+
     }
+
+
+
+
+
+
 
 }

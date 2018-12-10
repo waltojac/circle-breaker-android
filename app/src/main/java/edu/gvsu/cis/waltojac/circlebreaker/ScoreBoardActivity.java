d28cd7fdf9cee17e9cdcaa377f053d026e084a53
Jacob Walton <waltojac@mail.gvsu.edu>
2018-12-10 11:59:04 -0500

    Cleaned up imports.

--- app/src/main/java/edu/gvsu/cis/waltojac/circlebreaker/ScoreBoardActivity.java
+++ app/src/main/java/edu/gvsu/cis/waltojac/circlebreaker/ScoreBoardActivity.java
@@ -1,21 +1,8 @@
 package edu.gvsu.cis.waltojac.circlebreaker;
 
-import android.content.Intent;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
-
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.database.ChildEventListener;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
 import edu.gvsu.cis.waltojac.circlebreaker.dummy.ScoreContent;
 
